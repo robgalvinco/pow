52c6e71464b4ebbf2d55afe1c80a00010ad38dfa
Rob Galvin <rob@robgalvin.co>
2021-11-01 15:10:48 -0400

    kapow-admin

--- kapow/kapow-admin.js
+++ kapow/kapow-admin.js
@@ -1,8 +1,3 @@
-$( document ).ready(function() {
-    //find kapow sections being used
-    if((window.location.href.includes("manage/site_builder"))){
-        alert("In Site Admin");
-    
-    }
-})    
+
+alert("In Site Admin");
 
